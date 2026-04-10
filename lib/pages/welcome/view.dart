00720bcc2e7c522dfbf1e50868e90293970a8be9
15dd <69246156+15dd@users.noreply.github.com>
2026-04-10 17:23:53 +0800

    优化欢迎界面下节点选择的ui

--- lib/pages/welcome/view.dart
+++ lib/pages/welcome/view.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:hikari_novel_flutter/pages/welcome/controller.dart';
 import 'package:hikari_novel_flutter/router/route_path.dart';
-import 'package:hikari_novel_flutter/service/local_storage_service.dart';
 import 'package:hikari_novel_flutter/widgets/state_page.dart';
 import '../../models/common/wenku8_node.dart';
 
 class WelcomePage extends StatelessWidget {
-  const WelcomePage({super.key});
+  WelcomePage({super.key});
+
+  final controller = Get.put(WelcomeController());
 
   @override
   Widget build(BuildContext context) {
+    final primaryColor = Theme.of(context).colorScheme.primary;
+
     return Scaffold(
       body: Center(
         child: Column(
@@ -24,12 +28,22 @@ class WelcomePage extends StatelessWidget {
             FilledButton.icon(onPressed: () => Get.toNamed(RoutePath.login), label: Text("go_to_login".tr), icon: const Icon(Icons.login)),
             const SizedBox(height: 40),
             PopupMenuButton<Wenku8Node>(
-              onSelected: (Wenku8Node value) {
-                LocalStorageService.instance.setWenku8Node(value);
-              },
+              onSelected: (Wenku8Node value) => controller.changeWenku8Node(value),
               itemBuilder: (BuildContext context) => [
-                PopupMenuItem<Wenku8Node>(value: Wenku8Node.wwwWenku8Net, child: Text(Wenku8Node.wwwWenku8Net.node)),
-                PopupMenuItem<Wenku8Node>(value: Wenku8Node.wwwWenku8Cc, child: Text(Wenku8Node.wwwWenku8Cc.node)),
+                PopupMenuItem<Wenku8Node>(
+                  value: Wenku8Node.wwwWenku8Net,
+                  child: Text(
+                    Wenku8Node.wwwWenku8Net.node,
+                    style: controller.wenku8Node == Wenku8Node.wwwWenku8Net ? TextStyle(color: primaryColor, fontWeight: FontWeight.bold) : null
+                  ),
+                ),
+                PopupMenuItem<Wenku8Node>(
+                  value: Wenku8Node.wwwWenku8Cc,
+                  child: Text(
+                    Wenku8Node.wwwWenku8Cc.node,
+                    style: controller.wenku8Node == Wenku8Node.wwwWenku8Cc ? TextStyle(color: primaryColor, fontWeight: FontWeight.bold) : null
+                  ),
+                ),
               ],
               child: Row(
                 mainAxisSize: MainAxisSize.min,
